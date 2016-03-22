d9a91831f53035ecd2950ea8900baa39636441c4
pavel <itasarom@gmail.com>
2016-03-22 22:07:19 +0300

    Started using boost

--- p3/3-proxy/proxy_main.cpp
+++ p3/3-proxy/proxy_main.cpp
@@ -1,5 +1,10 @@
 #include "boost/asio.hpp"
+#include "proxy.hpp"
 
 int main (int argc, char **argv){
+
+    boost::asio::io_service io_service;
+    Proxy p("", io_service);
+    p.run();
     return 0;
 }
