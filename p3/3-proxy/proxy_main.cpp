3153a6fce36d9eb5616485ea9c0453b6d209d316
pavel <itasarom@gmail.com>
2016-03-23 20:58:35 +0300

    Managed to accept connections

--- p3/3-proxy/proxy_main.cpp
+++ p3/3-proxy/proxy_main.cpp
@@ -1,9 +1,10 @@
 #include "boost/asio.hpp"
 #include "proxy.hpp"
+#include <memory.h>
 
 int main (int argc, char **argv){
 
-    boost::asio::io_service io_service;
+    auto io_service = std::make_shared<Proxy::io_service_t>();
     Proxy p("", io_service);
     p.run();
     return 0;
